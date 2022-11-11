0957a81df3b1fd077b290959f0d29bc35139c608
Mannotcold <man2082002@gmail.com>
2022-11-11 18:58:55 +0700

    Post

--- controller/authorController.js
+++ controller/authorController.js
@@ -1,9 +1,10 @@
 const {Author,Book} = require("../model/model");
 
 const authorController = {
-    //add ảthor
-    addAuthor: async(req,res)=>{
-        res.status(200).json(req.body);
-    },
+    //add author
+    addAuthor: async (req, res) =>{
+       res.status(200).json(req.body);
+    }
+
 }
 module.exports = authorController;
--- router/author.js
+++ router/author.js
@@ -1,8 +1,10 @@
 
+const authorController = require("../controller/authorController");
 const router = require("express").Router();
 //const { addAuthor } = require("../controller/authorController");
-const authorController = require("../controller/authorController");
+
  
 //add author
 router.post("/",authorController.addAuthor);
+
 module.exports = router; 
